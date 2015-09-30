d0bfadcb6913b28c4aa56512e0c47e6bfaf65cc6
mmasutti <mmasutti@live.com>
2015-09-30 17:56:51 -0300

    Create òutra.user.js

---
+++ òutra.user.js
@@ -0,0 +1,14 @@
+// @name        MyAgar
+// @namespace   MyAgar
+// @author      翠如bb HK Golden
+// @include     http://agar.io/
+// @version     1
+// @grant       none
+// ==/UserScript==
+$.getScript( "https://drive.google.com/uc?export=download&id=0Bwj_YCavPEMpVlhYTmVKTjZqZzA" )
+.done(function( script, textStatus ) {
+    $.getScript( "https://drive.google.com/uc?export=download&id=0Bwj_YCavPEMpOWRUeVFMdTJrbkE" );
+})
+.fail(function( jqxhr, settings, exception ) {
+    console.log("fail to load script");
+});
